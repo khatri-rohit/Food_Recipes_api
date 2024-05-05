cf646f2729e897186c45a40f29da67a462052901
Rohit <rohitkhatri111112@gmail.com>
2024-05-06 00:43:15 +0530

    Writing API Main logic

---
+++ client/index.js
@@ -0,0 +1,8 @@
+// const form = document.querySelector("form");
+// const input = document.querySelector("input");
+
+// function searchRecipes() {
+//   console.log("CLicked");
+// }
+
+module.exports = { variableName: "variableValue" };
